7ae39771b3703a6c4a6606cba87f060e952dc6ee
José Eduardo Brandão <zefutbrandao@gmail.com>
2021-05-02 11:00:43 -0300

    script basico criado

--- Assets/Ladder.cs
+++ Assets/Ladder.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ladder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public float speed = 1;
+    
+    private void OnTriggerStay2D(Collider2D other)
     {
-        
+        if (other.tag == "Player" && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        } else  if (other.tag == "Player" && (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
     }
 }
