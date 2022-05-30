4a494914a41567995803a20e16e20f532577f5c8
José <josevictorjop@gmail.com>
2022-05-30 00:45:21 -0300

    Added Freny Runner

---
+++ Assets/Runner/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RPlayer : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float Yincrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+    public Text healthDisplay;
+
+    public GameObject gameOver;
+
+    private void Update()
+    {
+        healthDisplay.text = "HP: " + health.ToString();
+
+        if (health <= 0) {
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight) {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight) {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+    }
+}
---
+++ Assets/Runner/Scripts/RepeatingBG.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBG : MonoBehaviour
+{
+    public float speed;
+
+    public float endX;
+    public float startX;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime); 
+
+        if(transform.position.x <= endX) {
+            Vector2 pos = new Vector2(startX, transform.position.y);
+            transform.position = pos;
+        }       
+    }
+}
