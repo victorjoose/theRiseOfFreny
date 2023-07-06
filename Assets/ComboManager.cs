967e18cce5aa6dca6205bf158f4a885555a87b6f
Jose Eduardo Brandao <zefutbrandao@gmail.com>
2023-07-05 22:44:05 -0300

    blink coroutine

--- Assets/ComboManager.cs
+++ Assets/ComboManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Globalization;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,22 +11,42 @@ public class ComboManager : MonoBehaviour
     private float timeRemaining;
     private Text comboText;
     private float combo = 1.0f;
+    private Color originalColor;
     // Start is called before the first frame update
     void Start()
     {
         timeRemaining = totalTime;
         comboText = GetComponent<Text>();
+        originalColor = comboText.color;
     }
 
     // Update is called once per frame
     void Update()
     {
+        StartCoroutine(BlinkText());
         timeRemaining -= Time.deltaTime;
 
         if (timeRemaining <= 0) {
             combo = 1.0f;
         } else {
-            comboText.text = combo.ToString();
+            comboText.text = combo.ToString("0.0", CultureInfo.InvariantCulture);
+        }
+    }
+
+
+    IEnumerator BlinkText() {
+        while (timeRemaining > 0)
+        {
+            // Calculate the alpha value based on the remaining time
+            float alpha = Mathf.Lerp(0.2f, 1f, timeRemaining / totalTime);
+
+            // Apply the modified color to the text component
+            comboText.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+
+            // Adjust the blinking speed based on the remaining time
+            float blinkSpeed = Mathf.Lerp(0.5f, 0.1f, timeRemaining / totalTime);
+
+            yield return new WaitForSeconds(blinkSpeed);
         }
     }
 
@@ -37,6 +58,15 @@ public class ComboManager : MonoBehaviour
 
     public void SetCombo(float combo) {
         this.combo = combo;
+    }
+
+    public float GetTimeRemaining() {
+        return timeRemaining;
+    }
+
+
+    public void SetTimeRemaining(float newTime) {
+        this.timeRemaining = newTime;
     }
 
 }
