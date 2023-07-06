2ca5efe99f17acc4d08c1778d622d5ced7284974
Jose Eduardo Brandao <zefutbrandao@gmail.com>
2023-07-05 22:35:39 -0300

    combo manager to text

--- Assets/ComboManager.cs
+++ Assets/ComboManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComboManager : MonoBehaviour
 {
+
+    public float totalTime = 60f;
+    private float timeRemaining;
+    private Text comboText;
+    private float combo = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeRemaining = totalTime;
+        comboText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeRemaining -= Time.deltaTime;
+
+        if (timeRemaining <= 0) {
+            combo = 1.0f;
+        } else {
+            comboText.text = combo.ToString();
+        }
     }
+
+
+    public float GetCombo() {
+        return combo;
+    }
+
+
+    public void SetCombo(float combo) {
+        this.combo = combo;
+    }
+
 }
